e2f51279e296d4845685c50ae83200c224791815
Albarrán Moreno Miguel <10599779@thyssenkrupp.gtk.local>
2020-04-27 09:55:51 +0200

    Seccion 8 terminada

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -5,16 +5,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const pagesRoutes: Routes = [
     {
         path: '',
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'account-settings', component: AccountSettingsComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'graficas1', component: Graficas1Component},
+            { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes del tema' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
+            { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
     },
